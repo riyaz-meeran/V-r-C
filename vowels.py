fceb9921b15c58b472ed6d6b33666b6ebc6e40e0
riyaz-meeran <42993283+riyaz-meeran@users.noreply.github.com>
2018-09-18 16:18:06 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,7 @@
+n=input()
+if n in ('a','e','i','o','u','A','E','I','O','U'):
+    print("Vowel")
+elif n in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'):
+        print("Consonant")
+else:
+    print("invalid")
